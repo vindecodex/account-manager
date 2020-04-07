5c8bbc48ff9553245474867c9eb4e62e44dddbca
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-07 16:16:55 +0800

    use the protect middleware on protected routes

--- routes/account.js
+++ routes/account.js
@@ -7,15 +7,15 @@ const auth = require('../controllers/auth');
 const router = express.Router();
 
 router.route('/')
-.get(accountController.getAccounts)
+.get(auth.protect, accountController.getAccounts)
 .post(auth.createAccount)
 
 router.route('/login')
 .post(auth.login)
 
 router.route('/:id')
-.get(accountController.getAccount)
-.patch(accountController.updateAccount)
-.delete(accountController.deleteAccount)
+.get(auth.protect, accountController.getAccount)
+.patch(auth.protect, accountController.updateAccount)
+.delete(auth.protect, accountController.deleteAccount)
 
 module.exports = router;
