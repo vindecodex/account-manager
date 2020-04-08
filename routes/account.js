c119aae731dbc99a4b47199e548116e9972f4eed
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-08 17:44:33 +0800

    removed first auth protect

--- routes/account.js
+++ routes/account.js
@@ -6,16 +6,30 @@ const auth = require('../controllers/auth');
 // We use mounting Routes
 const router = express.Router();
 
+// router.route('/')
+// .get(auth.protect, accountController.getAccounts)
+// .post(auth.createAccount)
+//
+// router.route('/login')
+// .post(auth.login)
+//
+// router.route('/:id')
+// .get(auth.protect, accountController.getAccount)
+// .patch(auth.protect, accountController.updateAccount)
+// .delete(auth.protect, accountController.deleteAccount)
+//
+//
+//
 router.route('/')
-.get(auth.protect, accountController.getAccounts)
+.get(accountController.getAccounts)
 .post(auth.createAccount)
 
 router.route('/login')
 .post(auth.login)
 
 router.route('/:id')
-.get(auth.protect, accountController.getAccount)
-.patch(auth.protect, accountController.updateAccount)
-.delete(auth.protect, accountController.deleteAccount)
+.get(accountController.getAccount)
+.patch(accountController.updateAccount)
+.delete(accountController.deleteAccount)
 
 module.exports = router;
