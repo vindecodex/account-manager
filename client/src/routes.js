e5fd643b9fc047c6b8ff2198936fa061148e3b1e
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-08 17:43:53 +0800

    guarded routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from './store'
 
 import Login from './components/Login'
 import Register from './components/Register'
+import Dashboard from './components/Dashboard'
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history',
   routes: [
-	{ path: '', component: Login },
-	{ path: '/register', component: Register }
+	{ 
+	  path: '',
+	  component: Login,
+	  // to where it want to go (url)
+	  // from where it came from (url)
+	  // next proceed to a url
+	  beforeEnter(to, from, next) {
+		// check if token exist
+		if(store.state.token) {
+		  next('/dashboard')
+		} else {
+		  next()
+		}
+	  }
+	},
+	{ 
+	  path: '/register',
+	  component: Register,
+	  beforeEnter(to, from, next) {
+		if(store.state.token) {
+		  next('/dashboard')
+		} else {
+		  next()
+		}
+	  }
+	},
+	{
+	  path: '/dashboard',
+	  component: Dashboard,
+	  beforeEnter(to, from, next) {
+		if(store.state.token) {
+		  next()
+		} else {
+		  next('')
+		}
+	  }
+	}
   ]
 });
