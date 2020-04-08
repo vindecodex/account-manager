691e0f81118a1ffa62de9303794b302437dddaae
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-08 17:43:26 +0800

    -added store -set axios baseURL -set devtools to true

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes'
+import store from './store'
+import axios from 'axios'
+
+// we use this so that we only need to use /accounts or /anything  everytime we request
+axios.defaults.baseURL = '/api/v1'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
+
 
 new Vue({
   router,
   render: h => h(App),
+  store
 }).$mount('#app')
