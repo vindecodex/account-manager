b48738520b83817a43b741e102f23e6f18810e58
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-09 13:40:36 +0800

    fix path importing store

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes'
-import store from './store'
+import store from './store/store'
 import axios from 'axios'
 
 // we use this so that we only need to use /accounts or /anything  everytime we request
