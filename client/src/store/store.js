fefffea6d5b863ae084ca3294904b2fd3bf307e0
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-13 13:05:18 +0800

    separated auth and account logic

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+import authAccount from './auth-account'
 import account from './account'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
+	authAccount,
 	account
   }
 })
