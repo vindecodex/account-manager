2021c060ede3ca96b4a3bdefe46f9bf11094d903
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-13 13:04:43 +0800

    added more functions for getting and updating accounts

--- client/src/store/account.js
+++ client/src/store/account.js
@@ -1,83 +1,86 @@
 import axios from 'axios'
 
-// accessing fields and methods in here to components or outside of store use below:
-// accessing fields this.$store.fieldname
-// accessing actions this.$store.dispatch(methodname, { parameters })
-// accessing mutations this.$store.commit(methodname, { parameters })
-// Note mostly actions used on components or routes - does not slow the frontend
-// Note mostly mutations used on actions - does slow the frontend
-
 export default {
   namespace: true,
   state: {
-	token: null,
+	accounts: null,
 	account: null
   },
   mutations: {
-	// mutations can be called by using commit('methodname')
 
-	SET_TOKEN(state, token) {
-	  state.token = token
+	// mutations can be called by using commit('methodname')
+	
+	SET_ACCOUNTS(state, accounts) {
+	  state.accounts = accounts
 	},
+
 	SET_ACCOUNT(state, account) {
 	  state.account = account
 	}
   },
   actions: {
+
 	// actions can be called by using dispatch('methodname')
 	// inside this object you can pass { commit, dispatch, state } if you wanted
-	register({ commit, state, dispatch }, registrationInput) {
+	// since our get accounts requires token headers but now we don't need to set it here because we already set it up during authentication on our subscriber.
+	
+	get_accounts({ commit, getters }) {
 	  return new Promise((resolve, reject) => {
-		axios.post('/accounts', registrationInput)
-		  .then(res => { 
-			dispatch('attempt', res.data.token)
-			resolve(res)
-		  })
-		  .catch(err => {
-			console.log(err)
-			reject(err)
-		  });
+	  axios.get('/accounts')
+	  .then(res => {
+		const accounts = res.data.accounts
+		commit('SET_ACCOUNTS', accounts)
+		resolve(getters.get_accounts)
+	  })
+	  .catch(err => {
+		console.log(err)
+		reject(err)
+	  })
 	  })
 	},
-	attempt({ commit, state }, token) {
-	  if (token) {
-		commit('SET_TOKEN', token)
-		// we run subscriber cuase we listen to mutation
-		// which can be found on ./store/subscriber
-	  }
 
-	  if (!state.token) {
-		return
-	  }
-
-	  axios.post('/accounts/authenticate')
+	get_account({ commit, getters }, id) {
+	  return new Promise((resolve, reject) => {
+		axios.get('/accounts/' + id)
 		.then(res => {
-		  commit('SET_ACCOUNT', res.data.account)
+		  const account = res.data.account
+		  commit('SET_ACCOUNT', account)
+		  resolve(getters.get_account)
 		})
 		.catch(err => {
 		  console.log(err)
-		  commit('SET_TOKEN', null)
-		  commit('SET_ACCOUNT', null)
+		  reject(err)
 		})
+	  })
 	},
-	login({ commit, dispatch }, loginInput) {
+
+	edit_account({ commit }, editInput) {
 	  return new Promise((resolve, reject) => {
-	  axios.post('/accounts/login', loginInput)
+		axios.patch('/accounts/' + editInput.id, { username: editInput.username })
 		.then(res => {
-		  dispatch('attempt', res.data.token)
+		  console.log(res)
+		  commit('SET_ACCOUNT', res.data.account)
 		  resolve(res)
 		})
 		.catch(err => {
 		  console.log(err)
 		  reject(err)
-		});
+		})
 	  })
 	}
+
   },
   getters: {
+
 	// getters for getting state fields on routes or components
-	getState(state) {
-	  return state
+	
+	get_accounts(state) {
+	  return state.accounts
+	},
+
+	get_account(state) {
+	  return state.account
 	}
+
   }
 }
