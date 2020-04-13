6c694729d380e4f26961d083795b8c80317f1d6c
Vincent Villaluna <villalunavincent@gmail.com>
2020-04-13 13:03:43 +0800

    imported fontawesome

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,10 @@ import { router } from './routes'
 import store from './store/store'
 import axios from 'axios'
 
+// FontAwesome
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 require('./store/subscriber')
 
 // we use this so that we only need to use /accounts or /anything  everytime we request
